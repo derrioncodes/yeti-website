72cb921434a04a71b4394fedf03c3e343fc85582
derrioncodes <derrioncodes@gmail.com>
2025-02-28 15:58:51 -0700

    added hover for collection images

---
+++ yeti-static-website/app.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+//     $(".collection-image").mouseover(function(){
+//       $("p").css("background-color", "yellow");
+//     });
+//     $("collection-image").mouseout(function(){
+//       $("p").css("background-color", "lightgray");
+//     });
+//   });
