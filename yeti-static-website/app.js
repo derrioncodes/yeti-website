e8d31974b7d44f9b1c708ea87cd0733f391a528a
derrioncodes <derrioncodes@gmail.com>
2025-03-01 11:56:10 -0700

    added scroll overlay

--- yeti-static-website/app.js
+++ yeti-static-website/app.js
@@ -1,8 +1,12 @@
-// $(document).ready(function(){
-//     $(".collection-image").mouseover(function(){
-//       $("p").css("background-color", "yellow");
-//     });
-//     $("collection-image").mouseout(function(){
-//       $("p").css("background-color", "lightgray");
-//     });
-//   });
+$(window).on('scroll', function() {
+  $('.full-image-collection-banner-text').each(function() {
+    let elementTop = $(this).offset().top;
+    let windowBottom = $(window).scrollTop() + $(window).height();
+    if (elementTop < windowBottom) {
+      $(this).addClass('visible');
+    }
+  });
+});
+
+
+  
