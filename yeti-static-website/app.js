edd3be61f82ab53915caca08634a93f2cc9164fc
derrioncodes <derrioncodes@gmail.com>
2025-03-02 07:49:11 -0700

    fixed transtion for banner

--- yeti-static-website/app.js
+++ yeti-static-website/app.js
@@ -1,6 +1,8 @@
 $(window).on('scroll', function() {
   $('.full-image-collection-banner-text').each(function() {
-    let elementTop = $(this).offset().top;
+    let elementTop = $(this).offset().top + 300;
+    console.log(elementTop);
+    
     let windowBottom = $(window).scrollTop() + $(window).height();
     if (elementTop < windowBottom) {
       $(this).addClass('visible');
