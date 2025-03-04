b5850b43e2ed9832aa6da98d2169756a6ead8aa8
derrioncodes <derrioncodes@gmail.com>
2025-03-03 17:23:08 -0700

    two column section and finished carousel 1

--- yeti-static-website/app.js
+++ yeti-static-website/app.js
@@ -1,14 +1,68 @@
 $(window).on('scroll', function() {
   $('.full-image-collection-banner-text').each(function() {
     let elementTop = $(this).offset().top + 300;
-    console.log(elementTop);
-    
     let windowBottom = $(window).scrollTop() + $(window).height();
     if (elementTop < windowBottom) {
       $(this).addClass('visible');
     }
   });
 });
+
+$(window).on('scroll', function() {
+  $('.two-column-homepage-leftside-text').each(function() {
+    let elementTop = $(this).offset().top + 300;
+    let windowBottom = $(window).scrollTop() + $(window).height();
+    if (elementTop < windowBottom) {
+      $(this).addClass('visible');
+    }
+  });
+});
+
+$(window).on('scroll', function() {
+  $('.two-column-homepage-rightside-text').each(function() {
+    let elementTop = $(this).offset().top + 300;
+    let windowBottom = $(window).scrollTop() + $(window).height();
+    if (elementTop < windowBottom) {
+      $(this).addClass('visible');
+    }
+  });
+});
+
+$(document).ready(function(){
+  $('.carousel-wrapper').slick({
+    initialSlide: 0,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    centerMode: false,
+    centerPadding: '40px',
+    arrows: true,
+    dots: false,
+
+
+  });
+
+  var slidesCount = $('.slick-slide').length;
+  // $('.slick-counter').html(slidesCount);
+  
+  countSlide();
+  $('.slick-slider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+      countSlide();
+  });
+  function countSlide() {
+      var current = $('.slick-slider').find('.slick-current').attr('data-slick-index');
+
+      if(current != 18){
+
+      }
+      $('.count').html(Number(current) + 1 );
+
+      
+  }
+ 
+});
+
+
 
 
   
