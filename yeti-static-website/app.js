5c28bbcfa066c691448113524c8f3b8527779e7a
derrioncodes <derrioncodes@gmail.com>
2025-03-04 19:42:52 -0700

    added second banner with no bg overlay

--- yeti-static-website/app.js
+++ yeti-static-website/app.js
@@ -1,5 +1,5 @@
 $(window).on('scroll', function() {
-  $('.full-image-collection-banner-text').each(function() {
+  $('.full-image-collection-banner-text.show-overlay, .full-image-collection-banner-text.show-overlay_no-bg-gradient').each(function() {
     let elementTop = $(this).offset().top + 300;
     let windowBottom = $(window).scrollTop() + $(window).height();
     if (elementTop < windowBottom) {
@@ -7,9 +7,11 @@ $(window).on('scroll', function() {
     }
   });
 });
+
+// 2 column homepage section
 
 $(window).on('scroll', function() {
-  $('.two-column-homepage-leftside-text').each(function() {
+  $('.two-column-homepage-leftside-text, .two-column-homepage-rightside-text').each(function() {
     let elementTop = $(this).offset().top + 300;
     let windowBottom = $(window).scrollTop() + $(window).height();
     if (elementTop < windowBottom) {
@@ -18,150 +20,9 @@ $(window).on('scroll', function() {
   });
 });
 
-$(window).on('scroll', function() {
-  $('.two-column-homepage-rightside-text').each(function() {
-    let elementTop = $(this).offset().top + 300;
-    let windowBottom = $(window).scrollTop() + $(window).height();
-    if (elementTop < windowBottom) {
-      $(this).addClass('visible');
-    }
-  });
-});
-
-// $(document).ready(function(){
-  
-//   $('.carousel-wrapper').slick({
-//     initialSlide: 0,
-//     infinite: true,
-//     slidesToShow: 5,
-//     slidesToScroll: 1,
-//     centerMode: false,
-//     centerPadding: '40px',
-//     arrows: true,
-//     dots: false,
 
 
-//   });
-
-//   var slidesCount = $('.slick-slide').length;
-//   $('.slick-counter').html(slidesCount);
-  
-//   countSlide();
-//   $('.slick-slider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
-//       countSlide();
-//   });
-//   function countSlide() {
-//       var current = $('.slick-slider').find('.slick-current').attr('data-slick-index');
-
-//       if(current != 18){
-
-//       }
-//       $('.count').html(Number(current) + 1 );
-
-      
-//   }
- 
-// });
-
-// $(document).ready(function(){
-
-//   let $carouselNodes = $('.carousel-section');
-  
-//   if($carouselNodes != null){
-//     $carouselNodes.each((node) =>{
-//       console.log(node.querySelectorAll('.carousel-wrapper'));
-      
-      
-      
-
-      // node.querySelectorAll('.carousel-wrapper').slick({
-      //   initialSlide: 0,
-      //   infinite: true,
-      //   slidesToShow: 5,
-      //   slidesToScroll: 1,
-      //   centerMode: false,
-      //   centerPadding: '40px',
-      //   arrows: true,
-      //   dots: false,
-    
-    
-      // });
-    
-      // var slidesCount = $('.slick-slide').length;
-      // $('.slick-counter').html(slidesCount);
-      
-      // countSlide();
-      // $('.slick-slider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
-      //     countSlide();
-      // });
-      // function countSlide() {
-      //     var current = $('.slick-slider').find('.slick-current').attr('data-slick-index');
-    
-      //     if(current != 18){
-    
-      //     }
-      //     $('.count').html(Number(current) + 1 );
-    
-          
-      // }
-      
-//     })
-//   }
-  
-  
- 
-// });
-
-
-// GPT
-
-
-// $(document).ready(function(){
-
-//   let $carouselNodes = $('.carousel-section');
-  
-//   if ($carouselNodes.length) { // More accurate check than `!= null`
-//     $carouselNodes.each((index, node) => { // node is the second argument
-
-//       node.querySelectorAll('.carousel-wrapper').slick({
-//         initialSlide: 0,
-//         infinite: true,
-//         slidesToShow: 5,
-//         slidesToScroll: 1,
-//         centerMode: false,
-//         centerPadding: '40px',
-//         arrows: true,
-//         dots: false,
-    
-    
-//       });
-    
-      
-//     });
-//   }
-  
-  
- 
-// });
-
-
-// var slidesCount = $('.slick-slide').length;
-      // $('.slick-counter').html(slidesCount);
-      
-      // countSlide();
-      // $('.slick-slider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
-      //     countSlide();
-      // });
-      // function countSlide() {
-      //     var current = $('.slick-slider').find('.slick-current').attr('data-slick-index');
-    
-      //     if(current != 18){
-    
-      //     }
-      //     $('.count').html(Number(current) + 1 );
-    
-          
-      // }
+// Carousel Section
 
 $(document).ready(function(){
   var helpers = {
