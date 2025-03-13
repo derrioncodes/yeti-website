d30ce55f46ba1ef3ad3b446e59009c00b3bd4c38
derrioncodes <derrioncodes@gmail.com>
2025-03-13 17:08:29 -0600

    made carousel and 2 column responsive

--- yeti-static-website/app.js
+++ yeti-static-website/app.js
@@ -108,6 +108,38 @@ $(document).ready(function(){
       centerPadding: '40px',
       arrows: true,
       dots: false,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 4
+          }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
     });
 
     if ($(this).find('.carousel-item').length > 1) {
